904db3a9d252c73d4d76341e807abeedadbe6108
Gercog <54477063+Gercog52@users.noreply.github.com>
2020-04-15 23:33:20 +0800

    CRX

--- cromeExtention/chromePacCRX.js
+++ cromeExtention/chromePacCRX.js
@@ -15,3 +15,30 @@ crx.load('./build')
     fs.writeFileSync('./foobar.crx', crxBuffer);
     console.log('все')
   });
+/*
+var fs = require('fs');
+var gulp = require('gulp');
+var crx = require('gulp-crx-pack');
+var manifest = require('./build/manifest.json');
+
+const NodeRSA = require('node-rsa');
+const key = new NodeRSA({b: 2048});
+
+fs.writeFileSync('key.pem',key.exportKey('pkcs1-pem'))
+
+gulp.task('crx', function() {
+  return gulp.src('./build')
+    .pipe(crx({
+      privateKey: key.exportKey('pkcs1-pem'),
+      filename: manifest.name + '.crx'
+    }))
+    .pipe(gulp.dest('./'));
+});
+
+function defaultTask(cb) {
+  console.log('start');
+  cb();
+}
+
+exports.default = defaultTask
+*/
