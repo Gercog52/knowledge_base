f5127a91ef1ee36b960f59613d68e706237ebcce
Gercog52 <54477063+Gercog52@users.noreply.github.com>
2020-11-27 23:36:02 +0800

    delete log

--- site/src/components/Md.js
+++ site/src/components/Md.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { graphql } from "gatsby"
 
 export default function Md(props) {
-  console.log(props);
   return (
     <div dangerouslySetInnerHTML={{
       __html:props.data.markdownRemark.html
